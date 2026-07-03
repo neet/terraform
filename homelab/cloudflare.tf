053afdf59fdb68fe41fa4edcc8ddf112cfb6b320
Ryo Igarashi <n33t5hin@gmail.com>
2026-07-04 02:45:09 +0900

    Add missing DNS record for open-webui

--- homelab/cloudflare.tf
+++ homelab/cloudflare.tf
@@ -16,6 +16,15 @@ resource "cloudflare_dns_record" "elasticsearch_neet_love" {
   proxied = true 
 }
 
+resource "cloudflare_dns_record" "open_webui_neet_love" {
+  zone_id = var.cloudflare_zone_id
+  name    = "open-webui"
+  content = "${data.cloudflare_zero_trust_tunnel_cloudflared.this.id}.cfargotunnel.com"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+}
+
 resource "cloudflare_zero_trust_tunnel_cloudflared_config" "this" {
   tunnel_id  = data.cloudflare_zero_trust_tunnel_cloudflared.this.id
   account_id = var.cloudflare_account_id 
@@ -34,7 +43,7 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "this" {
       {
         hostname       = "open-webui.neet.love"
         origin_request = {}
-        service        = "http://compute-mitaka-02.local:3000"
+        service        = "http://192.168.11.3:3000"
       },
       {
         service = "http_status:404"
